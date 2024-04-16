105f7c18fce14451f3af8c2146957e04b9787985
DStalkerBR <jhon89araujo@gmail.com>
2024-04-16 16:49:16 -0300

    Adiciona endpoint para criar conta

---
+++ src/Cepedi.Banco.Conta.Compartilhado/Requests/CriarContaRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using OperationResult;
+
+namespace Cepedi.Banco.Conta.Compartilhado;
+
+public class CriarContaRequest : IRequest<Result<CriarContaResponse>>
+{
+    public int IdPessoa { get; set; }
+    public required string Agencia { get; set; }
+    public required string Numero { get; set; }
+    public decimal Saldo { get; set; }
+}
---
+++ src/Cepedi.Banco.Conta.Dominio/Handlers/CriarContaRequestHandler.cs
@@ -0,0 +1,44 @@
+﻿using Cepedi.Banco.Conta.Compartilhado;
+using Cepedi.Banco.Conta.Compartilhado.Enums;
+using Cepedi.Banco.Conta.Dominio.Entidades;
+using Cepedi.Banco.Conta.Dominio.Repositorio;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+
+namespace Cepedi.Banco.Conta.Dominio.Handlers;
+
+public class CriarContaRequestHandler : IRequestHandler<CriarContaRequest, Result<CriarContaResponse>>
+{
+    private readonly ILogger<CriarContaRequestHandler> _logger;
+    private readonly IContaRepository _contaRepository;
+
+    public CriarContaRequestHandler(IContaRepository contaRepository, ILogger<CriarContaRequestHandler> logger)
+    {
+        _contaRepository = contaRepository;
+        _logger = logger;
+    }
+    public Task<Result<CriarContaResponse>> Handle(CriarContaRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var conta = new ContaEntity()
+            {
+                IdPessoa = request.IdPessoa,
+                Agencia = request.Agencia,
+                Numero = request.Numero,
+                Saldo = request.Saldo
+            };
+
+            _contaRepository.CriarContaAsync(conta);
+
+            return Result.Success(new CriarContaResponse(conta.IdConta, conta.Agencia, conta.Numero));
+        }
+        catch
+        {
+            _logger.LogError("Ocorreu um erro durante a execução");
+            return Result.Error<CriarContaResponse>(new Compartilhado.Excecoes.ExcecaoAplicacao(
+                (ContaMensagemErrors.ErroGravacaoConta)));
+        }
+    }
+}
