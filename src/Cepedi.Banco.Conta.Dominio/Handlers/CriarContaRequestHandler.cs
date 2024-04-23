d57f821e910670b01ba8e068a93217a30b9b2e34
DStalkerBR <jhon89araujo@gmail.com>
2024-04-23 20:23:31 -0300

    remove try catch

--- src/Cepedi.Banco.Conta.Dominio/Handlers/CriarContaRequestHandler.cs
+++ src/Cepedi.Banco.Conta.Dominio/Handlers/CriarContaRequestHandler.cs
@@ -1,5 +1,4 @@
 ﻿using Cepedi.Banco.Conta.Compartilhado;
-using Cepedi.Banco.Conta.Compartilhado.Enums;
 using Cepedi.Banco.Conta.Dominio.Entidades;
 using Cepedi.Banco.Conta.Dominio.Repositorio;
 using MediatR;
@@ -20,27 +19,18 @@ public class CriarContaRequestHandler : IRequestHandler<CriarContaRequest, Resul
     }
     public Task<Result<CriarContaResponse>> Handle(CriarContaRequest request, CancellationToken cancellationToken)
     {
-        try
+        var conta = new ContaEntity()
         {
-            var conta = new ContaEntity()
-            {
-                IdPessoa = request.IdPessoa,
-                Agencia = request.Agencia,
-                Numero = request.Numero,
-                Status = request.Status,
-                LimiteTrasancao = request.LimiteTrasancao,
-                LimiteCredito = request.LimiteCredito
-            };
+            IdPessoa = request.IdPessoa,
+            Agencia = request.Agencia,
+            Numero = request.Numero,
+            Status = request.Status,
+            LimiteTrasancao = request.LimiteTrasancao,
+            LimiteCredito = request.LimiteCredito
+        };
 
-            _contaRepository.CriarContaAsync(conta);
+        _contaRepository.CriarContaAsync(conta);
 
-            return Result.Success(new CriarContaResponse(conta.Id, conta.Agencia, conta.Numero));
-        }
-        catch
-        {
-            _logger.LogError("Ocorreu um erro durante a execução");
-            return Result.Error<CriarContaResponse>(new Compartilhado.Excecoes.ExcecaoAplicacao(
-                (ContaMensagemErrors.ErroGravacaoConta)));
-        }
+        return Result.Success(new CriarContaResponse(conta.Id, conta.Agencia, conta.Numero));
     }
 }
