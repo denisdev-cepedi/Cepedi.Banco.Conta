b67dc42a74b75d87b5ea37d75ed9cdb0228e83f9
DStalkerBR <jhon89araujo@gmail.com>
2024-05-20 11:09:37 -0300

    Add atualizar conta (status e limites)

---
+++ src/Cepedi.Banco.Conta.Compartilhado/Validators/AtualizarContaRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using Cepedi.Banco.Conta.Compartilhado.Enums;
+using Cepedi.Banco.Conta.Compartilhado.Requests;
+using FluentValidation;
+
+namespace Cepedi.Banco.Conta.Compartilhado.Validators;
+
+public class AtualizarContaRequestValidator : AbstractValidator<AtualizarContaRequest>
+{
+    public AtualizarContaRequestValidator()
+    {
+        RuleFor(x => x.IdConta)
+            .GreaterThan(0)
+            .WithMessage("IdConta deve ser maior que 0");
+
+        RuleFor(x => x.Status)
+            .IsInEnum()
+            .WithMessage($"Status deve ser um dos valores númericos: {string.Join(", ", Enum.GetValues<EStatusConta>().Cast<EStatusConta>().Select(x => $"{(int)x} - {x}"))}");
+
+        RuleFor(x => x.LimiteTrasancao)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("LimiteTrasancao deve ser maior ou igual a 0");
+
+        RuleFor(x => x.LimiteCredito)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("LimiteCredito deve ser maior ou igual a 0");
+    }
+
+}
---
+++ src/Cepedi.Banco.Conta.Dominio/Handlers/AtualizarContaRequestHandler.cs
@@ -0,0 +1,39 @@
+﻿using Cepedi.Banco.Conta.Compartilhado.Enums;
+using Cepedi.Banco.Conta.Compartilhado.Requests;
+using Cepedi.Banco.Conta.Compartilhado.Responses;
+using Cepedi.Banco.Conta.Dominio.Repositorio;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+
+namespace Cepedi.Banco.Conta.Dominio.Handlers;
+public class AtualizarContaRequestHandler : IRequestHandler<AtualizarContaRequest, Result<AtualizarContaResponse>>
+{
+    private readonly ILogger<AtualizarContaRequestHandler> _logger;
+    private readonly IContaRepository _contaRepository;
+
+    public AtualizarContaRequestHandler(IContaRepository contaRepository, ILogger<AtualizarContaRequestHandler> logger)
+    {
+        _contaRepository = contaRepository;
+        _logger = logger;
+    }
+    public async Task<Result<AtualizarContaResponse>> Handle(AtualizarContaRequest request, CancellationToken cancellationToken)
+    {
+        var contaExistente = await _contaRepository.ObterContaAsync(request.IdConta);
+
+        if (contaExistente == null)
+        {
+            return Result.Error<AtualizarContaResponse>(new Compartilhado.Excecoes.ExcecaoAplicacao(
+                (ContaMensagemErrors.ContaNaoExiste)));
+        }
+
+        contaExistente.Status = request.Status;
+        contaExistente.LimiteTrasancao = request.LimiteTrasancao;
+        contaExistente.LimiteCredito = request.LimiteCredito;
+
+        await _contaRepository.AtualizarContaAsync(contaExistente);
+
+        return Result.Success(new AtualizarContaResponse(contaExistente.Id, contaExistente.Status, contaExistente.LimiteTrasancao, contaExistente.LimiteCredito));
+    }
+
+}
